dc7a1a3088cdcfd2bf3a083f51b88dbbfb8892d5
Bobbo Fuxx <134005319+BobboFuxx@users.noreply.github.com>
2025-04-16 13:21:05 -0700

    Update mint.rs

--- src/mint.rs
+++ src/mint.rs
@@ -1,8 +1,24 @@
 use crate::types::{NFTMetadata, NFT};
 use crate::utils::{hash_metadata_commitment, generate_nft_id};
+use snarkjs::{groth16, Circuit, FieldElement};
+
+pub fn generate_metadata_commitment_proof(metadata: &NFTMetadata) -> String {
+    let name_hash = hash_metadata_commitment(&metadata.name);
+    let desc_hash = hash_metadata_commitment(&metadata.description);
+    let image_cid_hash = hash_metadata_commitment(&metadata.image_cid);
+    let attr_hash = hash_metadata_commitment(&metadata.attributes);
+
+    let proof = groth16::generate_proof(
+        &Circuit::load("metadata_commitment.circom"), 
+        &[FieldElement::from(name_hash), FieldElement::from(desc_hash), FieldElement::from(image_cid_hash), FieldElement::from(attr_hash)]
+    ).unwrap();
+
+    proof.to_hex()
+}
 
 pub fn mint_nft(owner: String, metadata: NFTMetadata) -> NFT {
     let commitment = hash_metadata_commitment(&metadata);
+    let proof = generate_metadata_commitment_proof(&metadata);
     let id = generate_nft_id();
 
     NFT {
