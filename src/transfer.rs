d3a9d8dccfd8cd0a1b4eb351268755b21ef80ded
Bobbo Fuxx <134005319+BobboFuxx@users.noreply.github.com>
2025-04-16 13:21:43 -0700

    Updat

--- src/transfer.rs
+++ src/transfer.rs
@@ -1,5 +1,14 @@
 use crate::types::NFT;
 
-pub fn transfer_nft(nft: &mut NFT, new_owner: String) {
+pub fn transfer_nft(nft: &mut NFT, new_owner: String, nullifier: Option<String>) {
+    if let Some(n) = nullifier {
+        store_nullifier(&n);
+    }
+
     nft.owner = new_owner;
+}
+
+fn store_nullifier(nullifier: &str) {
+    // Store nullifier to track that this NFT has been used
+    println!("Storing nullifier: {}", nullifier);
 }
