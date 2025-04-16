dbbec7d8e954eb49da0651ee3059cb158d6fc23a
Bobbo Fuxx <134005319+BobboFuxx@users.noreply.github.com>
2025-04-16 13:34:10 -0700

    Update view.rs

--- src/view.rs
+++ src/view.rs
@@ -1,14 +1,6 @@
-use crate::types::NFT;
+use crate::{state::NFTState, types::NFT};
 
-pub fn transfer_nft(nft: &mut NFT, new_owner: String, nullifier: Option<String>) {
-    if let Some(n) = nullifier {
-        store_nullifier(&n);
-    }
-
-    nft.owner = new_owner;
-}
-
-fn store_nullifier(nullifier: &str) {
-    // Store nullifier to track that this NFT has been used
-    println!("Storing nullifier: {}", nullifier);
+pub fn reveal_nft(state: &NFTState, id: &str, viewing_key: Option<String>) -> Option<&NFT> {
+    // For now, we're not using the viewing_key in logic, but it could be added in zk circuit.
+    state.get_nft(id)
 }
