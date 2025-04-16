ca3ea3c3d2d85db8e8f81e942f037dbb3d90bd7d
Bobbo Fuxx <134005319+BobboFuxx@users.noreply.github.com>
2025-04-16 13:26:03 -0700

    Update types.rs

--- src/types.rs
+++ src/types.rs
@@ -1,1 +1,17 @@
-pub use crate::nft::{NFT, NFTMetadata};
+#[derive(Clone)]
+pub struct NFTMetadata {
+    pub name: String,
+    pub description: String,
+    pub image_cid: String,
+    pub attributes: String,
+    pub shielded: bool,
+}
+
+#[derive(Clone)]
+pub struct NFT {
+    pub id: String,
+    pub owner: String,
+    pub metadata_commitment: String,
+    pub royalty_percentage: Option<u8>,
+    pub staking_locked: bool,
+}
