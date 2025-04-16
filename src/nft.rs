2b407ce9761a1bb9306d058ed36caba457062750
Bobbo Fuxx <134005319+BobboFuxx@users.noreply.github.com>
2025-04-16 13:29:53 -0700

    Update nft.rs

--- src/nft.rs
+++ src/nft.rs
@@ -1,14 +1,26 @@
-use crate::types::{NFTMetadata, NFT};
-use crate::mint::mint_nft;
-use crate::utils::{hash_metadata_commitment, generate_nft_id};
+use std::collections::HashMap;
+use crate::types::NFT;
 
-pub fn create_nft(owner: String, metadata: NFTMetadata) -> NFT {
-    let commitment = hash_metadata_commitment(&metadata);
-    let id = generate_nft_id();
-    mint_nft(owner, metadata)
+pub struct NFTState {
+    pub nfts: HashMap<String, NFT>,
 }
 
-pub fn update_nft_metadata(nft: &mut NFT, new_metadata: NFTMetadata) {
-    let new_commitment = hash_metadata_commitment(&new_metadata);
-    nft.metadata_commitment = new_commitment;
+impl NFTState {
+    pub fn new() -> Self {
+        Self {
+            nfts: HashMap::new(),
+        }
+    }
+
+    pub fn add_nft(&mut self, nft: NFT) {
+        self.nfts.insert(nft.id.clone(), nft);
+    }
+
+    pub fn get_nft(&self, id: &str) -> Option<&NFT> {
+        self.nfts.get(id)
+    }
+
+    pub fn get_nft_mut(&mut self, id: &str) -> Option<&mut NFT> {
+        self.nfts.get_mut(id)
+    }
 }
