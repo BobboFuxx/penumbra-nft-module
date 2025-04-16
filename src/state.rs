717ae21b63585f42d23a3e8a23bb2df1c9841a63
Bobbo Fuxx <134005319+BobboFuxx@users.noreply.github.com>
2025-04-16 13:29:07 -0700

    Update state.rs

--- src/state.rs
+++ src/state.rs
@@ -1,27 +1,26 @@
-// Basic state management (stub for example)
 use std::collections::HashMap;
+use crate::types::NFT;
 
 pub struct NFTState {
     pub nfts: HashMap<String, NFT>,
 }
 
-#[derive(Clone)]
-pub struct NFT {
-    pub id: String,
-    pub owner: String,
-    pub metadata_commitment: String,
-    pub royalty_percentage: Option<u8>,
-    pub staking_locked: bool,
-}
-
 impl NFTState {
     pub fn new() -> Self {
-        NFTState {
+        Self {
             nfts: HashMap::new(),
         }
     }
-    
+
     pub fn add_nft(&mut self, nft: NFT) {
         self.nfts.insert(nft.id.clone(), nft);
+    }
+
+    pub fn get_nft(&self, id: &str) -> Option<&NFT> {
+        self.nfts.get(id)
+    }
+
+    pub fn get_nft_mut(&mut self, id: &str) -> Option<&mut NFT> {
+        self.nfts.get_mut(id)
     }
 }
