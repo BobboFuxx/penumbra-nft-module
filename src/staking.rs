7cf78e7bce79999c863a0a8c4d4a73b63c1eb717
Bobbo Fuxx <134005319+BobboFuxx@users.noreply.github.com>
2025-04-16 13:24:04 -0700

    Update staking.rs

--- src/staking.rs
+++ src/staking.rs
@@ -2,8 +2,10 @@ use crate::types::NFT;
 
 pub fn stake_nft(nft: &mut NFT) {
     nft.staking_locked = true;
+    println!("NFT {} has been staked.", nft.id);
 }
 
 pub fn unstake_nft(nft: &mut NFT) {
     nft.staking_locked = false;
+    println!("NFT {} has been unstaked.", nft.id);
 }
